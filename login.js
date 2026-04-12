894a93bb42a0ad48cd4b2d2afea86188457f38e6
skywalker113t <hubertarene23@gmail.com>
2026-04-12 01:33:10 +0100

    alooot

---
+++ login.js
@@ -0,0 +1,12 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+  
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+  
+    if (email === "test@gmail.com" && password === "123456") {
+      window.location.href = "index.html";
+    } else {
+      alert("Invalid login");
+    }
+  });
