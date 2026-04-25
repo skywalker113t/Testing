36bc3f80256630b688f33bc57d63f1c9f21b5a11
skywalker113t <hubertarene23@gmail.com>
2026-04-25 09:27:44 +0100

    something happened

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc' // Make sure it says -swc
 
 export default defineConfig({
-  plugins: [react()]
-});
+  plugins: [react()],
+})
