3219483e37bbfae4264095dec21ce17e57711db7
skywalker113t <hubertarene23@gmail.com>
2026-06-03 03:20:16 +0100

    new settings icon yaayy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 import logo from "../../images/E.png";
 import profileImage from "../../images/eeh.jpg";
 import settingsIcon from "../../images/download1.png";
-import notificationIcon from "../../images/download.png";
+import notificationIcon from "../../images/download 2.png";
 
 export default function Navbar({ currentTheme }) {
   return (
